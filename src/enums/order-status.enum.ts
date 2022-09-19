c4d0957660d4d9cd99aa094b2fddae69334dedae
Tobias Jüschke <tobias.jueschke@bike24.net>
2022-09-19 11:18:03 +0200

    Init project

---
+++ src/enums/order-status.enum.ts
@@ -0,0 +1,9 @@
+export enum OrderStatus {
+  NEW = 'new',
+  PAYMENT_PENDING = 'payment_pending',
+  SHIPPING_AUTHORIZED = 'shipping_authorized',
+  COMPLETED = 'completed',
+  ACCEPTED = 'accepted',
+  SHIPPED = 'shipped',
+  PARTIALLY_SHIPPED = 'partially_shipped',
+}
